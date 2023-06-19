03e033465c2dba7a9e1164ed0cb1175122a6212e
Stanislav Prykhodko <stasprykhodko@gmail.com>
2023-06-19 20:39:20 +0300

    one more image display improved

--- src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -199,7 +199,7 @@ const ShoppingCartPage = () => {
                     {cartData.map(item => (
                       <div className="orederedDish" key={item.id}>
                         <div className="divForImage">
-                          <img className="dishImage" src={item.image} alt={item.name} />
+                          <img className="dishImage" src={`/new-delivery-mongo-express/${item.image}`} alt={item.name} />
                         </div>
                         <div className="infoAndHandling">
                           <div className="currentInfo">
